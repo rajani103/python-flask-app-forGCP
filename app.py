4268b4e72d5f65488552168eae4d557e183968a1
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2025-04-05 16:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    message = os.getenv("WELCOME_MSG", "Welcome to Cloud Run demo!")  # default if env var not set
     return jsonify({
-        "message": "Welcome to Cloud Run demo!",
+        "message": message,
         "status": "success"
     })
 
 @app.route('/health')
 def health():
     return jsonify({"status": "healthy"})
+
+@app.route('/env')
+def env_demo():
+    owner = os.getenv("APP_OWNER", "Not set")
+    return jsonify({
+        "environment": {
+            "APP_OWNER": owner
+        }
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
